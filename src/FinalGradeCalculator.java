d38cb3d788f563967a44130560ba0301664fe374
Veduren <veduren7@gmail.com>
2019-04-25 19:31:41 -0400

    Added validity check for user input

--- src/FinalGradeCalculator.java
+++ src/FinalGradeCalculator.java
@@ -1,38 +1,73 @@
-/*
- * This program will calculate the grade you need to get on your final
- * Author: Ved
- */
+//This program will calculate the grade you need to get on your final
 
 import java.util.Scanner;
 
 public class FinalGradeCalculator {
-	
+
 	public static void main(String[] args) {
-		
+
 		//Creating a new Scanner object
 		Scanner user_input = new Scanner(System.in);
-		
+
+		//Initializing variables
+		double CurrentGrade = 0;
+		double DesiredGrade = 0;
+		double FinalWorth = 0;
+
 		try {
+
+			//Informing the user of the function of the program
+			System.out.println("This program will calculate the grade you need to get on your final");
 			
-		//Informing the user of the function of the program
-		System.out.println("This program will calculate the grade you need to get on your final");
-		
-		//User enters Current Grade
-		System.out.print("What is your current average (%): ");
-		double CurrentGrade = user_input.nextDouble();
-		
-		//User enters desired grade
-		System.out.print("What is your desired grade (%): ");
-		int DesiredGrade = Integer.parseInt(user_input.next());
-		
-		//User enters final worth
-		System.out.print("What is the weight of your final (%): ");
-		double FinalWorth = user_input.nextDouble();
-		
-		//Calculate Minimum grade to get on the final
-		double MinimumFinalGrade = (100*(DesiredGrade - CurrentGrade*((100-FinalWorth)/(double) 100)))/(double) FinalWorth;
-		System.out.println("You will need at least: " +MinimumFinalGrade + " %");
-		
+			//Loop checks validity of user input: Current average
+			while(true) {
+				System.out.print("What is your current average (%): ");
+				if(user_input.hasNextDouble()) {
+					CurrentGrade = user_input.nextDouble();
+					if(CurrentGrade > 0 && CurrentGrade < 100) {
+						break;
+					}
+				}
+				else {
+					user_input.next(); //clearing the previous value in the scanner
+				}
+				System.out.println("Invalid Entry! Please enter a number [0,100]");
+			}	
+
+			//Loop checks validity of user input: Desired average
+			while(true) {
+				System.out.print("What is your desired grade (%): ");
+				if(user_input.hasNextDouble()) {
+					DesiredGrade = user_input.nextDouble();
+					if(DesiredGrade > 0 && DesiredGrade < 100) {
+						break;
+					}
+				}
+				else {
+					user_input.next();
+				}
+				System.out.println("Invalid Entry! Please enter a number [0,100]");
+			}
+			
+			//Loop checks validity of user input: Final Worth
+			while(true) {
+				System.out.print("What is the weight of your final (%): ");
+				if(user_input.hasNextDouble()) {
+					FinalWorth = user_input.nextDouble();
+					if(FinalWorth > 0 && FinalWorth < 100) {
+						break;
+					}
+				}
+				else {
+					user_input.next();
+				}
+				System.out.println("Invalid Entry! Please enter a number [0,100]");
+			}
+
+			//Calculate Minimum grade to get on the final
+			double MinimumFinalGrade = (100*(DesiredGrade - CurrentGrade*((100-FinalWorth)/(double) 100)))/(double) FinalWorth;
+			System.out.println("You will need at least: " +MinimumFinalGrade + " %");
+
 		}
 		finally {
 			user_input.close();
